df75477b6f3ee7662475e044fb50003ab3152a49
cmsbuild <>
2010-01-20 09:43:28 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- ShmReader-V01-05-00-01'.
    Sprout from master 2010-01-20 09:43:27 UTC Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch> 'do not set evtstate to processing for LUMISECTION events'
    Cherrypick from EventFilter-ShmReader-backport333 2010-01-20 09:42:22 UTC Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch> 'do not set evtstate to processing for LUMISECTION events':
        src/FUShmReader.cc

--- src/FUShmReader.cc
+++ src/FUShmReader.cc
@@ -93,7 +93,7 @@ int FUShmReader::fillRawData(EventID& eID,
   }
   else if(state==evt::LUMISECTION){
     unsigned int ls = newCell->getLumiSection();
-    shmBuffer_->setEvtState(newCell->index(),evt::PROCESSING);
+    //    shmBuffer_->setEvtState(newCell->index(),evt::PROCESSING);
     shmBuffer_->scheduleRawCellForDiscard(newCell->index());
     return (-1)*ls;
   }
@@ -114,7 +114,7 @@ int FUShmReader::fillRawData(EventID& eID,
   
   // reading the cell is finished (new state will be 'isProcessing')
   shmBuffer_->finishReadingRawCell(newCell);
-  eID=EventID(runNumber_,1U,evtNumber_);
+  eID=EventID(runNumber_,evtNumber_);
   data=event_;
   
   return evtNumber_;
